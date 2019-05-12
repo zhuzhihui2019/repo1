26ea583dbf6e0c10f50590dd7b5a553c5013e6e8
itcast <bos@itcast.cn>
2019-05-12 20:36:11 +0800

    新建User.java,提交到本地版本库

---
+++ git_start_demo/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+/**
+ * @program: git_start_demo
+ * @description
+ * @author: allen
+ * @params
+ * @create: 2019-05-12 20:27
+ **/
+public class User {
+    private String name;
+    private Integer age;
+}
